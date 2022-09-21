a6eaade7b7b1d70dafa8346aa182c62058377221
won0324 <huiwon0324@naver.com>
2022-09-22 00:09:20 +0900

    fix: App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
@@ -8,22 +7,10 @@ import DevelopersPage from "./pages/DevelopersPage";
 import ProjectsPage from "./pages/ProjectsPage";
 import DevelopersdetailPage from "./pages/DevelopersdetailPage";
 import ProjectsdetailPage from "./pages/ProjectsdetailPage";
-=======
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/LandingPage';
-import Navbar from './component/Navbar';
-import AboutPage from './pages/AboutPage';
-import DevelopersPage from './pages/DevelopersPage';
-import ProjectsPage from './pages/ProjectsPage';
-import DevelopersdetailPage from './pages/DevelopersdetailPage'
-import ProjectsdetailPage from './pages/ProjectsdetailPage'
->>>>>>> 503a7a9390f6b41d485581e46501aec8181cb7c1
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
       <Router>
         <Routes>
           <Route path="/" element={<Navbar />}>
@@ -39,20 +26,6 @@ function App() {
           </Route>
         </Routes>
       </Router>
-=======
-    <Router>
-      <Routes>
-        {/*<Route path ="/" element={<Navbar />}>*/}
-          <Route path="/" element={<LandingPage />}/>
-          <Route path="/about" element={<AboutPage />}/>
-          <Route path="/developers" element={<DevelopersPage />}/>
-          <Route path="/projects" element={<ProjectsPage />}/>
-          <Route path="/developers/detail" element={<DevelopersdetailPage />}/>
-          <Route path="/projects/detail" element={<ProjectsdetailPage />}/>
-        {/*</Route>*/}
-      </Routes>
-    </Router>
->>>>>>> 503a7a9390f6b41d485581e46501aec8181cb7c1
     </>
   );
 }
