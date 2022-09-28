5a3af1f4d5bf59a6bf0d241be03dfd85c945990d
yellow-jam <88462774+yellow-jam@users.noreply.github.com>
2022-09-28 22:36:08 +0900

    AboutPage 수정, DevelopersdetailPage 요청 연결 (#7)
    * proxy 설정, DevelopersdetailPage와 AboutPage의 css에서 id와 class 구분, DevelopersdetailPage에서 개발자 상세 정보 요청(미완성)
    
    * AboutPage 제목 박스 위치 조정
    
    * DevelopersdetailPage 요청 연결 마무리, css 수정

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -5,14 +5,14 @@ function AboutPage() {
   return (
     <>
     
-      <div className={styles.titleContainer}>
-        <div className={styles.title}>
+      <div id={styles.titleContainer}>
+        <div id={styles.title}>
         ABOUT COMPUTER ENGINEERING
         </div>
       </div>
       
-      <div className={styles.contentContainer}>
-        <div className={styles.aboutContainer}>
+      <div id={styles.contentContainer}>
+        <div id={styles.aboutContainer}>
           <div className={styles.aboutTitle}>창의적인 사고와 실무능력 배양</div>
           <p className={styles.aboutContent}>논리적인 사고력과 창의성을 강조하는 교육과 교과내용을 통해 단순 지식에 그치지 않고 이를 컴퓨터 정보통신 등 IT 산업 전반의 실무 능력에 적용하는 능력을 배양한다.</p>
           <div className={styles.aboutTitle}>엄격하고 심도 있는 교과 관리</div>
@@ -21,7 +21,7 @@ function AboutPage() {
           <p className={styles.aboutContent}>전공 분야에 대한 지식과 리더십 습득을 기반으로 직업인으로서 가져야할 장인정신을 배양하여, 자신의 존재 가치를 확인할 수 있는 여성 전문인력을 배양한다.</p>
         </div>
       
-        <div className={styles.professorContainer}>
+        <div id={styles.professorContainer}>
           <div className={styles.professorCard}>
             {/* <div className={styles.professorImage} /> */}
             <p className={styles.professorName}>박우창 교수님</p>
