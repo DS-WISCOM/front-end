e63a19cd8a40240a94b03920e49ad7624033e5ce
unknown <lerle144@gmail.com>
2022-10-27 00:07:11 +0900

    fix: Pacman Spinner

--- src/component/Spinner.js
+++ src/component/Spinner.js
@@ -1,24 +1,25 @@
 import React from "react";
-import PacmanLoader from "react-spinners/PacmanLoader";
+import styles from "../css/Spinner.module.css";
 
-const Spinner = () => {
+const Spinner = (props) => {
+  
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "50px 0",
-      }}
-    >
-      <PacmanLoader
-        size={30}
-        margin={2}
-        color="#ffffff"
-        speed={1}
-      />
+    <div className={styles.box}>
+        <div className={styles.loader}>
+            <div className={styles.circles}>
+                <span className={styles.one}></span>
+                <span className={styles.two}></span>
+                <span className={styles.three}></span>
+            </div>
+            <div className={styles.pacman}>
+                <span className={styles.top}></span>
+                <span className={styles.bottom}></span>
+                <span className={styles.left}></span>
+                <div className={styles.eye}></div>
+            </div>
+        </div>
     </div>
-  );
+  )
 };
 
 export default Spinner;
